9f9c39f7b0159b6c3686ae81e44acb90ccb45601
raphy <raphy@ymail.com>
2023-02-23 17:08:00 -0800

    create a static web interface

--- Coding Exercise/CodingExercise18.py
+++ Coding Exercise/CodingExercise18.py
@@ -1,6 +1,6 @@
 import PySimpleGUI as sg
 
-
+sg.theme("Black")
 def convert(feet_input, Inches_input):
     Feet_meters = feet_input * 0.3048
     Inches_meters = Inches_input * 0.0254
@@ -15,21 +15,24 @@ Inches_label = sg.Text("Enter inches: ")
 Inches_input = sg.Input(key="inches")
 
 Conv_button = sg.Button("Convert")
+Exit_button = sg.Button("Exit")
 output_label = sg.Text("", key="output")
 
 window = sg.Window("Conversion to Meters", layout=[[feet_label, feet_input],
                                                    [Inches_label, Inches_input],
-                                                   [Conv_button, output_label]])
+                                                   [Conv_button, Exit_button, output_label]])
 
 while True:
     event, values = window.read()
+    feet = float(values["feet"])
+    inches = float(values["inches"])
 
-    try:
-        feet = float(values["feet"])
-        inches = float(values["inches"])
+    result = convert(feet, inches)
+    window["output"].update(value=f"{result} m", text_color="white")
+    match event:
+        case "Exit":
+            break
+        case sg.WIN_CLOSED:
+            break
 
-        result = convert(feet, inches)
-        window["output"].update(value=f"{result} m", text_color="white")
-    except ValueError:
-        sg.popup("Please provide two numbers.", font=("Helvetica", 20))
 window.close()
--- GUI.py
+++ GUI.py
@@ -1,13 +1,18 @@
 import functions
 import PySimpleGUI as sg
 import time
+import os
+
+if not os.path.exists("todos.txt"):
+    with open("todos.txt", 'w') as file:
+        pass
 
 sg.theme("Black")
 
 clock = sg.Text('', key='clock')
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
-add_button = sg.Button("Add")
+add_button = sg.Button(tooltip="Add todo", key="Add")
 list_box = sg.Listbox(values=functions.get_todos(), key='todos',
                       enable_events=True, size=[45, 10])
 edit_button = sg.Button("Edit")
@@ -22,7 +27,7 @@ window = sg.Window('My To-Do App',
                    font=('helvetica', 20))
 
 while True:
-    event, values = window.read(timeout=500)
+    event, values = window.read(timeout=10)
     window['clock'].update(value=time.strftime("%b %d, %Y %H: %M:%S"))
     match event:
         case "Add":
