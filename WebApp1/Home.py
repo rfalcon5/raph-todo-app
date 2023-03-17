fd6b5315eb5bdc4201b8d06864dc168051901d97
raphy <raphy@ymail.com>
2023-03-17 11:58:22 -0700

    Changed the page from web.py to Home.py

--- WebApp1/Home.py
+++ WebApp1/Home.py
@@ -2,6 +2,7 @@ import streamlit as st
 import functions
 
 todos = functions.get_todos()
+st.set_page_config(layout="wide")
 
 def add_todo():
     todo = st.session_state["new_todo"] + "\n"
@@ -13,7 +14,8 @@ def complete_todo():
 
 st.title("My Todo App")
 st.subheader("This is my todo app.")
-st.write("This app is to increase your productivity.")
+st.write("This app is to increase your <B> productivity </B>.",
+         unsafe_allow_html=True)
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
